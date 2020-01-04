9a59b88d9d4b0cd90fc5884ecfefff57f909d2be
Vladimir <sharkfire88@gmail.com>
2020-01-04 14:15:50 +0300

    final implentation;

--- main.cpp
+++ main.cpp
@@ -5,10 +5,15 @@ int main()
     state_machine fsm;
     fsm.initiate();
 
+    fsm.process_event(watchdog { 0 }); //will be unconsumed
     fsm.process_event(turn_on {});
+    fsm.process_event(watchdog { 1 });
     fsm.process_event(start {});
+    fsm.process_event(watchdog { 2 });
     fsm.process_event(finish {});
+    fsm.process_event(watchdog { 3 });
     fsm.process_event(turn_off {});
+    fsm.process_event(watchdog { 4 }); //will be unconsumed
 
     return 0;
 }
--- state_machine.h
+++ state_machine.h
@@ -3,8 +3,9 @@
 #include <iostream>
 
 #include <boost/statechart/state_machine.hpp>
-#include <boost/statechart/simple_state.hpp>
+#include <boost/statechart/state.hpp>
 #include <boost/statechart/transition.hpp>
+#include <boost/statechart/custom_reaction.hpp>
 
 #include <boost/mpl/list.hpp>
 
@@ -25,6 +26,10 @@ struct turn_off : sc::event< turn_off > {};
 struct turn_on  : sc::event< turn_on > {};
 struct start    : sc::event< start > {};
 struct finish   : sc::event< finish > {};
+struct watchdog : sc::event< watchdog > {
+  watchdog(uint16_t _sequence) : sequence(_sequence) { }
+  uint16_t sequence;
+};
 
 /**
  * Some template magic to merge common reactions for all states and derived
@@ -37,7 +42,20 @@ struct transitions_list : mpl::list<base_transitions...> {
     using merge = transitions_list<base_transitions..., derived_transitions...>;
 };
 
-typedef transitions_list<sc::transition<turn_off, off_state>> base_transtions;
+typedef transitions_list<sc::transition<turn_off, off_state>, sc::custom_reaction<watchdog>> base_reactions;
+
+template< class MostDerived, class Context, class InnerInitial = boost::mpl::list<>, sc::history_mode historyMode = sc::has_no_history >
+struct base_state : public sc::simple_state<MostDerived, Context, InnerInitial, historyMode >
+{
+    typedef base_reactions reactions;
+
+    sc::result react (const watchdog& event) {
+      return react_on_watchdog(event);
+    }
+
+protected:
+    virtual sc::result react_on_watchdog(const watchdog& event) = 0;
+};
 
 // States:
 struct off_state : sc::simple_state<off_state, state_machine> {
@@ -45,12 +63,22 @@ struct off_state : sc::simple_state<off_state, state_machine> {
   typedef mpl::list< sc::transition<turn_on, on_state>> reactions;
 };
 
-struct on_state : sc::simple_state<on_state, state_machine> {
+struct on_state : base_state<on_state, state_machine> {
   on_state () { std::cout << "In ON state" << std::endl; }
-  typedef base_transtions::merge< sc::transition<start, active_state> > reactions; 
+  typedef base_reactions::merge<sc::transition<start, active_state>> reactions;
+  
+  sc::result react_on_watchdog(const watchdog& event) {
+    std::cout << "on_state wathdog react sequence: " << event.sequence << std::endl;
+    return discard_event();
+  };
 };
 
-struct active_state : sc::simple_state<active_state, state_machine> {
+struct active_state : base_state<active_state, state_machine> {
   active_state () { std::cout << "In ACTIVE state" << std::endl; }
-  typedef base_transtions::merge< sc::transition<finish, on_state> > reactions;
+  typedef base_reactions::merge<sc::transition<finish, on_state> > reactions; 
+
+  sc::result react_on_watchdog(const watchdog& event) {
+    std::cout << "active_state wathdog react sequence: " << event.sequence << std::endl;
+    return discard_event();
+  };
 };
